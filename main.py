bb60cc7c38b98104ff4f28654034900f564914e6
manasvisuresh17 <manasvi_suresh17@outlook.com>
2025-04-15 12:31:30 +0530

    lesson 6 activity 2

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-#x=int(input("enter an input "))
-#print(type(x))
-
 x=4
 if type(x) is int:
     print("given number is an integer")
 else:
     print("it is a string")
+
+x=2.0
+if type(x) is float:
+    print("the number given is a float value")
+else:
+    print("the given number is not a float value")
+
+x=12
+y=13
+if x is not y:
+    print("they are different")
+else:
+    print("they are same")
 
 
 
